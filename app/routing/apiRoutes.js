f0aa8971062848e2fbf241089c2ff4d0217d6b19
Sams <nicholettesams@msn.com>
2018-10-22 21:20:04 -0400

    psuedo code some logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,33 @@
+var friends = require("../data/friends")
+
 
 app.get("/api/friends", function(req, res) {
-    //res.sendFile(path.join(__dirname, "home.html"));
+    res.json(friends)
   });
   
   app.post("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newcharacter = req.body;
+    var newFriend = req.body;
   
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    //do i need a route name?
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newFriend);
+
+    // add scores together of newFriend
+
+    var match = 0 // Saves the index of the best match
+    // loop through friends
+    // add scores together of friend (should I store a total??)
+    // save friend as best match if the different between newFriend is lower than the previous friend
+
+    
+    // after finding match, add the newFriend to the array, do this after so not to compare newFriend to themselves
+    freinds.push(newFriend);
   
-    console.log(newcharacter);
-  
-    characters.push(newcharacter);
-  
-    res.json(newcharacter);
+    res.json(friends[match]);
   });
+
+  // TO DO...how do I export this to be able to import into use in server.js?
