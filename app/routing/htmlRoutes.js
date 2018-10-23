3cebc4e90650b2d9d4cf90405609032fcddf0973
Sams <nicholettesams@msn.com>
2018-10-22 21:11:10 -0400

    fix paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,12 @@
+var path = require("path")
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
   
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
 });
+
+// TO DO...how do I export this to be able to import into use in server.js?
   
