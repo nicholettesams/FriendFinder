cacc0da16dd4a42aee7d2fa771f3188e60c471ad
Sams <nicholettesams@msn.com>
2018-10-22 21:05:06 -0400

    Change port for heroku.  add references to route files

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 // Dependencies
 var express = require("express");
+var app = express();
 var path = require("path");
 
-var app = express();
-var PORT = 3000;
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
